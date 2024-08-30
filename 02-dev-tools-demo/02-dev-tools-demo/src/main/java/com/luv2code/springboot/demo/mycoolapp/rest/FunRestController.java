fccbcd21b7452157555ae1ccfb212399375d667d
Shrivarsha <123290462+shrivarshapoojari@users.noreply.github.com>
2024-08-31 00:28:37 +0530

    Accessing application properties in rest controller

--- 02-dev-tools-demo/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ 02-dev-tools-demo/02-dev-tools-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,12 +1,28 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+   // inject properties from application properties
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value(("${team.name}"))
+    private String teamName;
     //expose "/" that return  "Hello World"
+
+// new endpoint for teaminfo
+
+    @GetMapping("/teaminfo")
+    public  String getTeamInfo(){
+        return "Coach" +coachName + "teamname" +teamName;
+    }
+
     @GetMapping("/")
     public  String sayHello()
     {
